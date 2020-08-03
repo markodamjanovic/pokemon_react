14d1eba1dd8c9f0cb854c21fd820486e923ef336
markodamjanovic <damjanovic.m1@gmail.com>
2020-08-03 21:02:58 +0200

    background image, css and html styling

--- pokemon_app/src/App.js
+++ pokemon_app/src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 export function App() {  
   return (
     <div className="App">
+      <div> <img src="/pokemon_logo.png" width="20%"/> </div>
+      <div className="App-body">
         <PokemonList/>
+      </div>
     </div>
   );
 }
