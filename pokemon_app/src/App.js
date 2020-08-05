fdbfbff387e64f1aad24a6c8341fd5f96547b007
markodamjanovic <damjanovic.m1@gmail.com>
2020-08-05 16:02:31 +0200

    moved images into img folder, removed css file, new css styling

--- pokemon_app/src/App.js
+++ pokemon_app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {PokemonList} from './features/pokemonList/PokemonList';
 import './App.css';
+import pokemonLogo from './img/pokemon_logo.png'
 
 export function App() {  
   return (
     <div className="App">
-      <div> <img src="/pokemon_logo.png" width="20%" alt=""/> </div>
+      <div> <img src={pokemonLogo} width="20%" alt=""/> </div>
       <div className="App-body">
         <PokemonList/>
       </div>
