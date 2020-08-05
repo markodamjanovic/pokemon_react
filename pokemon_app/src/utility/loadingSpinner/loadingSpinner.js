073314a25ed953d41fe7a7a37ea8cbfdfbfefb14
markodamjanovic <damjanovic.m1@gmail.com>
2020-08-05 20:23:57 +0200

    css responsive styling

--- pokemon_app/src/utility/loadingSpinner/loadingSpinner.js
+++ pokemon_app/src/utility/loadingSpinner/loadingSpinner.js
@@ -4,7 +4,7 @@ import pokeBall from '../../img/poke_ball.png'
 export function LoadingSpinner() {
     return (
         <div className="container">
-           <h1>Loading...</h1> <img className="loader" src={pokeBall} alt="" />
+           <h1 className="h1-spinner">Loading...</h1> <img className="loader" src={pokeBall} alt="" />
         </div>
     );
 }
