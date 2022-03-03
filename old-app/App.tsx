eb52498337b81ef363a6f52a9865262b515529ef
marko.damjanovic <marko.damjanovic@ngit.hr>
2022-03-03 21:28:36 +0100

    moved old app to backup file

---
+++ old-app/App.tsx
@@ -0,0 +1,18 @@
+/*import React from 'react';
+import {PokemonList} from './features/pokemonList/PokemonList';
+import './index.scss';
+//import pokemonLogo from './img/pokemon_logo.png'
+
+export function App() {  
+  return (
+    <div className="App">
+      
+      <div className="App-body">
+        <PokemonList/>
+      </div>
+    </div>
+  );
+}
+//<div> <img src={pokemonLogo} width="20%" alt=""/> </div>
+export default App;
+*/
