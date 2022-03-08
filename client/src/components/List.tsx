be09221c2a7c9a577c0542e7b009e203eda045a3
marko.damjanovic <marko.damjanovic@ngit.hr>
2022-03-08 19:58:16 +0100

    initial dispatch call

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -1,6 +1,17 @@
 import React, {useEffect, useState} from "react";
+import { useDispatch } from "react-redux";
+import { getAllPokemons } from "../app/listSlice";
 
 const List = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetch = async() => {
+            dispatch(getAllPokemons());
+        };
+
+        fetch();
+    }, []);
 
     return <div>
         This will be populated with pokemons!
