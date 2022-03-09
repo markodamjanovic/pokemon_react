a91eeb4931c7d2dd3d8a149e2122c4640430d0ea
marko.damjanovic <marko.damjanovic@ngit.hr>
2022-03-09 23:25:53 +0100

    dispatch of action on initial component load

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import Filter from './components/Filter';
-import List from './components/List';
+import { useDispatch } from "react-redux";
+import { getAllPokemons } from "./app/pokemonListSlice";
+import { waitOnServerSequence } from "./app/globalSlice";
+import PokemonList from "./components/PokemonList";
 
-const App = () => <div className='app-layout'>
-    <div className='logo-row'> 
-        <div className='logo-row__logo-column'></div> 
-    </div>
-    <div className='filter-row'> 
-        <div className="filter-row__filter-column"> <Filter/> </div>  
-        </div>
-    <div className='list-row'> 
-        <div className='list-row__list-cell'> 
-            <List/>
-        </div>
-    </div>
-</div>
+const App = () => {
+    const dispatch = useDispatch();
 
+    useEffect(() => {
+        const fetch = async () => { dispatch(waitOnServerSequence(getAllPokemons)) };
+        fetch();
+    }, []);
+
+    return (
+        <div className='app-layout'>
+            <div className='logo-row'> <div className='logo-row__logo-column'></div> </div>
+            <div className='filter-row'>
+                <div className="filter-row__filter-column"> <Filter /> </div>
+            </div>
+            <div className='list-row'>
+                <div className='list-row__list-cell'>
+                    <PokemonList />
+                </div>
+            </div>
+        </div>
+    );
+}
 export default App;
